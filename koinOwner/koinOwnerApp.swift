64478cfb76f60eabcb66f248ae9d7e3ae5cd0d10
KimNahun <rlaskgns0212@naver.com>
2024-06-30 05:26:19 +0900

    [add] 프로젝트 생성

---
+++ koinOwner/koinOwnerApp.swift
@@ -0,0 +1,17 @@
+//
+//  koinOwnerApp.swift
+//  koinOwner
+//
+//  Created by 김나훈 on 6/30/24.
+//
+
+import SwiftUI
+
+@main
+struct koinOwnerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
