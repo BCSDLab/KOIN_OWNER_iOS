e2b80cbf5b0f4e101446cc9c321ec4b738d419a3
KimNahun <rlaskgns0212@naver.com>
2024-07-04 17:33:57 +0900

    feat: register 에서 공통적으로 쓰이는 ProgressBar 구현

---
+++ koinOwner/Sources/Common/Components/CustomProgressBar.swift
@@ -0,0 +1,31 @@
+//
+//  CustomProgressBar.swift
+//  koinOwner
+//
+//  Created by 김나훈 on 7/4/24.
+//
+
+import SwiftUI
+
+struct CustomProgressBar: View {
+    
+    private let progress: Double
+
+    init(progress: Double) {
+        self.progress = progress
+    }
+    
+    var body: some View {
+        GeometryReader { geometry in
+            ZStack(alignment: .leading) {
+                RoundedRectangle(cornerRadius: 4)
+                    .frame(height: 3)
+                    .foregroundColor(Color.neutral200)
+                RoundedRectangle(cornerRadius: 4)
+                    .frame(width: geometry.size.width * CGFloat(progress), height: 3)
+                    .foregroundColor(Color.main500)
+            }
+        }
+        .frame(height: 3)
+    }
+}
