be3e36b74a8f9c00913989c6243fa8827bf94c35
KimNahun <rlaskgns0212@naver.com>
2024-07-07 19:19:51 +0900

    chore: UIScreen -> geometry

--- koinOwner/Sources/Common/Components/CustomProgressBar.swift
+++ koinOwner/Sources/Common/Components/CustomProgressBar.swift
@@ -16,14 +16,16 @@ struct CustomProgressBar: View {
     }
     
     var body: some View {
+        GeometryReader { geometry in
             ZStack(alignment: .leading) {
                 RoundedRectangle(cornerRadius: 4)
                     .frame(height: 3)
                     .foregroundStyle(Color.neutral200)
                 RoundedRectangle(cornerRadius: 4)
-                    .frame(width: UIScreen.main.bounds.width * CGFloat(progress), height: 3)
+                    .frame(width: geometry.size.width * CGFloat(progress), height: 3)
                     .foregroundStyle(Color.main500)
             }
+        }
         .frame(height: 3)
     }
 }
