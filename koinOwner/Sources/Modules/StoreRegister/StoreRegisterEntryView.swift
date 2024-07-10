e6b0da8d3d47f1fb0d4fc0a82274332ac6d4bc9e
JeongYeongJoon <71048593+JeongYeongJoon@users.noreply.github.com>
2024-07-10 13:58:10 +0900

    feat: CustomNavigationBar 제작

---
+++ koinOwner/Sources/Modules/StoreRegister/StoreRegisterEntryView.swift
@@ -0,0 +1,18 @@
+//
+//  StoreRegisterEntryView.swift
+//  koinOwner
+//
+//  Created by 정영준 on 7/10/24.
+//
+
+import SwiftUI
+
+struct StoreRegisterEntryView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    StoreRegisterEntryView()
+}
---
+++ koinOwner/Sources/Modules/StoreRegister/StoreRegisterView.swift
@@ -0,0 +1,18 @@
+//
+//  StoreRegisterView.swift
+//  koinOwner
+//
+//  Created by 정영준 on 7/10/24.
+//
+
+import SwiftUI
+
+struct StoreRegisterView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    StoreRegisterView()
+}
